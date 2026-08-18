392bbd87a4603d80032d1df881c1312d2c80babb
chadiboudaher <chadiboudaher7@gmail.com>
2026-08-18 18:25:20 +0300

    feat: test model on synthetic data and with sklearn model

--- implementations/multinominal-logistic-regression-from-scratch/multinominal_logistic_regression.py
+++ implementations/multinominal-logistic-regression-from-scratch/multinominal_logistic_regression.py
@@ -1,4 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+
+RANDOM_SEED = 42
 
 def softmax(z):
     """
@@ -74,9 +81,62 @@ class MultinominalLogisticRegression:
 
     def plot_loss(self):
         """Plot training loss history"""
-        import matplotlib.pyplot as plt
         plt.plot(self.loss_history)
         plt.xlabel("Epoch")
         plt.ylabel("Loss")
         plt.title("Training Loss")
         plt.show()
+
+np.random.seed(RANDOM_SEED)
+
+X, y = make_classification(
+    n_samples=1000,
+    n_features=8,
+    n_classes=4,
+    n_informative=4,
+    n_redundant=2,
+    n_clusters_per_class=1,
+    class_sep=2.0,
+    flip_y=0.0,
+    random_state=42
+)
+
+X_train, X_test, y_train, y_test = (
+    train_test_split(
+        X, y, test_size=0.2, random_state=RANDOM_SEED, shuffle=True
+    )
+)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+my_model = MultinominalLogisticRegression(
+    n_features=8,
+    n_classes=4
+)
+my_model.fit(X_train, y_train, epochs=800, lr=0.1)
+
+sk_model = LogisticRegression(max_iter=800)
+sk_model.fit(X_train, y_train)
+
+
+y_pred = my_model.predict(X_test)
+predictions = my_model.predict_prob(X_test)
+
+y_pred_sk = sk_model.predict(X_test)
+predictions_sk = sk_model.predict_proba(X_test)
+
+accuracy = np.mean(y_pred == y_test)
+accuracy_sk = np.mean(y_pred_sk == y_test)
+print(f"Test Accuracy: {accuracy}")
+print(f"Test Accuracy SKLEARN: {accuracy_sk}")
+
+OHE = OneHotEncoder(sparse_output=False)
+y_test_onehot = OHE.fit_transform(y_test.reshape(-1, 1))
+loss = compute_loss(y_test_onehot, predictions)
+loss_sk = compute_loss(y_test_onehot, predictions_sk)
+print(f"Test Loss: {loss}")
+print(f"Test Loss SKLEARN: {loss_sk}")
+
+my_model.plot_loss()
