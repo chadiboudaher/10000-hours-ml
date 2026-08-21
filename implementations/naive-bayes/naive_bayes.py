75fe3549340cc1f6f2174b48c4b862847f314ad7
chadiboudaher <chadiboudaher7@gmail.com>
2026-08-21 17:03:20 +0300

    feat: add fit() means, variance, and prior variables

---
+++ implementations/naive-bayes/naive_bayes.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+
+N_CLASSES = 5
+N_SAMPLES = 500
+N_FEATURES = 5
+
+# Input Data
+X = np.random.randn(N_SAMPLES, N_FEATURES)
+
+# Output Data
+y = np.random.randint(0, N_CLASSES, N_SAMPLES)
+
+for i in range(N_CLASSES):
+    mask = y == i
+    X[mask] += np.random.randn(N_FEATURES) * 2
+
+print(f"input data X shape: {X.shape}")
+print(f"Output data y shape: {y.shape}")
+print(f"Input data X sample: {X[0]}")
+print(f"Output data y sample: {y[:10]}")
+
+class GaussianNaiveBayes:
+    def __init__(self):
+        self.classes_ = None
+        self.means_ = None # shape: (n_classes, n_features)
+        self.variances_ = None # shape: (n_classes, n_features)
+        self.priors_ = None # shape: (n_classes)
+
+    def fit(self, X, y):
+        total_samples = len(X)
+        n_samples, n_features = X.shape
+        n_classes = len(np.unique(y))
+
+        self.classes_ = np.unique(y)
+
+        self.means_ = np.zeros((n_classes, n_features))
+        self.variances_ = np.zeros((n_classes, n_features))
+        self.priors_ = np.zeros((n_classes))
+
+        for i, cls in enumerate(self.classes_):
+            X_cls = X[y == cls]
+
+            self.means_[i] = np.mean(X_cls, axis=0)
+            self.variances_[i] = np.var(X_cls, axis=0)
+            self.priors_[i] = len(X_cls) / n_samples
+
+        print(f"Means shape: {self.means_.shape}")
+        print(f"Variances shape: {self.variances_.shape}")
+        print(f"Priors: {self.priors_}")
+        print(f"Classes: {self.classes_}")
+
+GNB = GaussianNaiveBayes()
+GNB.fit(X, y)
