7d1c0e76f12833d55665202ef6c97c6aa2cb6f5f
chadiboudaher <chadiboudaher7@gmail.com>
2026-09-05 11:09:39 +0300

    feat: Load CIFAR-10 dataset, split dataset, and create DataLoader

--- experiments/residual-connections/experiment.py
+++ experiments/residual-connections/experiment.py
@@ -33,3 +33,39 @@ def set_seed(seed):
 set_seed(SEED)
 
 print("Device:", device)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(
+        (0.4914, 0.4822, 0.4465),
+        (0.2470, 0.2435, 0.2616)
+    )
+])
+
+train_dataset = torchvision.datasets.CIFAR10(
+    root="./data",
+    train=True,
+    download=True,
+    transform=transform
+)
+
+test_dataset = torchvision.datasets.CIFAR10(
+    root="./data",
+    train=False,
+    download=True,
+    transform=transform
+)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    num_workers=2
+)
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=False,
+    num_workers=2
+)
