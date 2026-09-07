d0f9c8807a0f0e090acf4ff52fe846c3a891634a
chadiboudaher <chadiboudaher7@gmail.com>
2026-09-07 12:17:12 +0300

    feat: add PyTorch media shapes

---
+++ pytorch/01. Shape/main.py
@@ -0,0 +1,13 @@
+import torch
+
+# Multiple shape found in Deep Learning
+
+# Images
+# [batch, channels, height, width]
+
+# Sequence
+# [batch, time, features]
+
+# Video
+# [batch, time, channels, height, width]
+
