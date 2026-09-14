f1be21518411600586ed84a1611c49a307c329ee
chadiboudaher <chadiboudaher7@gmail.com>
2026-09-14 12:43:08 +0300

    feat: create DelayedRecallDataset class

--- experiments/01_long_term_memory/dataset.py
+++ experiments/01_long_term_memory/dataset.py
@@ -10,7 +10,9 @@ Inputs:
 
 import torch
 import numpy as np
+from torch.utils.data import Dataset
 
+NUM_SAMPLES = 100
 RANDOM_SEED = 42
 SEQUENCE_LENGTH = 50
 NUM_CLASSES = 10
@@ -19,12 +21,59 @@ np.random.seed(RANDOM_SEED)
 
 # Create an example
 
-X = np.random.randint(0, NUM_CLASSES, SEQUENCE_LENGTH)
-y = X[0]
+# X = np.random.randint(0, NUM_CLASSES, SEQUENCE_LENGTH)
+# y = X[0]
 
-print(f"X shape: {X.shape}")
-print(f"X sample: {X[:5]}")
-X = torch.from_numpy(X)
-print(f"X tensor: {X[:5]}")
+# print(f"X shape: {X.shape}")
+# print(f"X sample: {X[:5]}")
+# X = torch.from_numpy(X)
+# print(f"X tensor: {X[:5]}")
 
-print(f"y value: {y}")
+# print(f"y value: {y}")
+
+class DelayedRecallDataset(Dataset):
+    def __init__(self,
+                 num_samples: int,
+                 num_classes: int,
+                 seq_length: int,
+                 random_seed: int):
+        super().__init__()
+
+        self.num_samples = num_samples
+        self.num_classes = num_classes
+        self.seq_length = seq_length
+        self.random_seed = random_seed
+
+        torch.manual_seed(self.random_seed)
+
+    def generate_dataset(self):
+        self.X = torch.randint(0,
+                          self.num_classes,
+                          size=(self.num_samples, self.seq_length))
+        self.y = self.X[:, 0]
+
+        return self.X, self.y
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
+
+dataset = DelayedRecallDataset(
+    NUM_SAMPLES,
+    NUM_CLASSES,
+    SEQUENCE_LENGTH,
+    RANDOM_SEED
+)
+
+X, y = dataset.generate_dataset()
+
+print(f"data shape: {X.shape}")
+print(f"X sample: {X[0]}")
+print(f"y value: {y[0]}")
+
+X_res, y_res = dataset.__getitem__(45)
+
+print(f"X res sample: {X_res}")
+print(f"y res value: {y_res}")
