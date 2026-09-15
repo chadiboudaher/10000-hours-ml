018c9988610e6a567ca723865bb8ee822ac7dc58
chadiboudaher <chadiboudaher7@gmail.com>
2026-09-15 18:49:44 +0300

    feat: implement GRU using PyTorch

--- experiments/01_long_term_memory/models.py
+++ experiments/01_long_term_memory/models.py
@@ -94,3 +94,37 @@ class LSTM(nn.Module):
         out, (hn, cn) = self.lstm(x, (h0, c0))
         out = self.fc(out[:, -1, :])
         return out
+
+
+class GRU(nn.Module):
+    def __init__(self,
+                 hidden_size, 
+                 output_size, 
+                 num_embedding,
+                 embedding_dim,
+                 num_layers=1):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.embedding = nn.Embedding(
+            num_embedding,
+            embedding_dim
+        )
+
+        self.gru = nn.GRU(
+            input_size=embedding_dim,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True
+        )
+
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        out, hn = self.gru(x, h0)
+        out = self.fc(out[:, -1, :])
+        return out
